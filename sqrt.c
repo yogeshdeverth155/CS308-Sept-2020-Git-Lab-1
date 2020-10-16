e853a101911af8bc6773411d1154ab567173472a
Yogesh Deverth <yogesh.deverth@gmail.com>
2020-10-16 14:35:05 +0530

    updated sqrt.c

--- sqrt.c
+++ sqrt.c
@@ -15,6 +15,7 @@ int main(int argc, char* argv[]) {
 	int input = atoi(argv[1]);
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting");
+	// to chekc whether the number is valid or not
 	return(0);
 
 } // end main
